57a9ae83293634bd557d8515438b9ed1dd619deb
Juan Garcia Alvite <juan@juangarcia.co.uk>
2021-05-28 08:21:44 +0100

    Add underscore to lower layer

--- keyboards/sofle/keymaps/foureight84/keymap.c
+++ keyboards/sofle/keymaps/foureight84/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | MOUSE|   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------| PLAY  |    |       |------+------+------+------+------+------|
- * | SHIFT|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  |      |
+ * | SHIFT|  =   |  _   |  -   |  +   |   {  |-------|    |-------|   }  |   [  |   ]  |   ;  |   :  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCTR |      |      |      | /       /       \NEXT  \  |      |      |      | RCTR |
  *            |      |      |      |      |/       /         \ WIND \ |      |      |      |      |
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
       KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
       KC_MOUSE, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-      _______,  KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_MPLY,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______, \
+      _______,  KC_EQL,  KC_UNDS, KC_MINS, KC_PLUS, KC_LCBR, KC_MPLY,       _______, KC_RCBR, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, \
                          KC_LCTRL, _______, _______, _______, _______,       KC_NWIND, _______, _______, _______, KC_RCTRL\
     ),
 
