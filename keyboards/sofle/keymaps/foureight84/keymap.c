f6380f94430be473a78a70f09090d4af5ddb6b61
Juan Garcia Alvite <juan@juangarcia.co.uk>
2021-10-25 21:17:42 +0100

    Add new layout

--- keyboards/sofle/keymaps/foureight84/keymap.c
+++ keyboards/sofle/keymaps/foureight84/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | LCTR |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | BSPC |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT( \
       KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,     KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC, \
       KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS, \
-      KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+      KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
       KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_MUTE,     XXXXXXX, KC_N,     KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
                        KC_LCTRL,  KC_LALT, KC_LGUI, KC_LOWER, KC_SPC,      KC_ENT,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTRL \
     ),
@@ -92,11 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10  | F11  | F12 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |  `   |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | MOUSE|   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+ * | MOUSE|      |      |      |  +   |      |-------.    ,-------|   {  |   }  |      |      |      |      |
  * |------+------+------+------+------+------| PLAY  |    |       |------+------+------+------+------+------|
- * | SHIFT|  =   |  _   |  -   |  +   |   {  |-------|    |-------|   }  |   [  |   ]  |   ;  |   :  |   \  |
+ * |      |  =   |  _   |  _   |  -   |      |-------|    |-------|   [  |   ]  |      |   ;  |   :  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |  [   |      |      |      | /       /       \NEXT  \  |      |      |      |   ]  |
  *            |      |      |      |      |/       /         \ WIND \ |      |      |      |      |
@@ -104,9 +104,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_LOWER] = LAYOUT( \
       KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-      KC_MOUSE, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-      _______,  KC_EQL,  KC_UNDS, KC_MINS, KC_PLUS, KC_LCBR, KC_MPLY,       _______, KC_RCBR, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, \
+      KC_GRV,   _______, _______, _______, _______,  _______,                        _______, _______, _______, _______, _______, _______, \
+      KC_MOUSE, _______, _______, _______, KC_MINS,  _______,                        KC_LCBR, KC_RCBR, _______, _______, _______, _______, \
+      _______,  KC_EQL,  KC_UNDS, KC_UNDS, KC_PLUS, _______, KC_MPLY,       _______, KC_LBRC, KC_RBRC, _______, KC_SCLN, KC_COLN, KC_BSLS, \
                          KC_LBRC, _______, _______, _______, _______,       KC_NWIND, _______, _______, _______, KC_RBRC\
     ),
 
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  ~   | INST | PSCR | MENU |      |      |                    | PGUP | PWRD |  UP  | NWRD | DLINE| BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | LALT | LCTL |LSHIFT|      | CAPS |-------.    ,-------| PGDN | LEFT | DOWN | RIGHT| DEL  | BSPC |
+ * | CAPS |      |      |      |      |      |-------.    ,-------| PGDN | LEFT | DOWN | RIGHT| DEL  | BSPC |
  * |------+------+------+------+------+------| SLEEP |    |       |------+------+------+------+------+------|
  * |      | UNDO | CUT  | COPY | PASTE|      |-------|    |-------|      | LSTR |      | LEND |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT( \
       KC_ESC,  KC_F13,  KC_F14,  KC_F15,  KC_F16,   KC_F17,                           KC_F18,  KC_F19,   KC_F20,  KC_F21,   KC_F22,   KC_DEL,  \
       KC_TILDE, KC_INS, KC_PSCR, KC_APP,  XXXXXXX,  XXXXXXX,                          KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC, \
-      _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,  KC_CAPS,                          KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC, \
+      KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,                          KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC, \
       _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX, KC_MACSLEEP,    _______, XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,  XXXXXXX,  _______, \
                         KC_LBRC, _______, _______, _______, KC_NAPP,        _______, _______, _______, _______, KC_RBRC \
     ),
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* MOUSE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |MS_BT1|MS_BT2|      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *             `----------------------------------'           '------''---------------------------'
  */
     [_MOUSE] = LAYOUT( \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_MOUSE_BTN1, KC_MOUSE_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_MOUSE_BTN1, KC_MOUSE_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
